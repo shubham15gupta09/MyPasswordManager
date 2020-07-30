d1e2c40f102cd0754c00dd4dc93f2624b8d2fd71
Shubham Gupta <shubham15gupta09@gmail.com>
2020-07-30 23:39:55 +0530

    changes in the error and success message

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Schema = require("./model/schema");
 
-mongoose.connect("mongodb+srv://rest:rest@cluster0.6sb1e.mongodb.net/<dbname>?retryWrites=true&w=majority",
-{ useUnifiedTopology: true , useNewUrlParser: true  },()=>{
-  console.log("connected to MongoDb successfully");
-});
+const url = `mongodb+srv://rest:rest@cluster0.6sb1e.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+mongoose.connect(
+  url,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("connected to MongoDb successfully");
+  }
+);
 
 app
   .set("view engine", "ejs")
@@ -24,72 +28,24 @@ app
     res.status(200).render("search.ejs");
   })
   .post("/submit", (req, res) => {
-    Schema
-    .create(req.body)
-    .then(r =>{
-      console.log(`success` + r);
-      res.status(200).send(`        <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Home</title>
-        </head>
-        <body style="padding: 5px;margin: 5px;"><p><b>Successfull</b></p><a href="/"><button>Go Back</button></a>        </body>
-        </html>`);
-    })
-    .catch(e =>{
-      console.log(`error` + e);
-      res.status(200).send(`        <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Home</title>
-        </head>
-        <body style="padding: 5px;margin: 5px;"><p><b>Error</b> + ${e} </p><a href="/save"><button>Try Again</button></a>        </body>
-        </html>`);
-    })
+    Schema.create(req.body)
+      .then((r) => {
+        res.status(200).render("sucess.ejs");
+      })
+      .catch((e) => {
+        res.status(200).render("error.ejs",{message:e});
+      });
   })
-  .post("/search",(req,res)=>{
+  .post("/search", (req, res) => {
     console.log(req.body);
-    Schema
-    .find(req.body)
-    .select("username password")
-    .then(r =>{
-      console.log(`found` + r);
-      console.log(r)
-      res.status(200).send(
-        `
-        <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
-  </head>
-  <body style="padding: 5px;margin: 5px;">
-        <p><b>Found</b></p><p>Username : ${r[0].username}</p><p>Password : ${r[0].password}</p><a href="/"><button>Go Back</button></a>
-        </body>
-        </html>
-        `);
-    })
-    .catch(e =>{
-      console.log(`error` + e);
-      res.status(200).send(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Home</title>
-        </head>
-        <body style="padding: 5px;margin: 5px;">      
-      <p><b>Error</b> + ${e} </p><a href="/save"><button>Try Again</button></a>
-      </body>
-      </html> 
-      `);
-    })
+    Schema.find(req.body)
+      .select("username password")
+      .then((r) => {
+        res.status(200).render("result.ejs",{data:r});
+      })
+      .catch((e) => {
+        res.status(200).render("error.ejs",{message:e});
+      });
   })
   .listen(PORT, () => {
     console.log(`server started on the port : ${PORT}`);
